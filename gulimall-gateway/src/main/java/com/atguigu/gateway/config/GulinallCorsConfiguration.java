294e17f40429a7238e679b1f7221cf558b0c0f64
lphx <1580780206@qq.com>
2020-08-21 15:46:36 +0800

    菜单

---
+++ gulimall-gateway/src/main/java/com/atguigu/gateway/config/GulinallCorsConfiguration.java
@@ -0,0 +1,30 @@
+package com.atguigu.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @Author: Penghong Li
+ * @Date: Create in 15:02 2020/8/21
+ */
+@Configuration
+public class GulinallCorsConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        //1.配置跨域
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+        source.registerCorsConfiguration("/**",corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+
+}
